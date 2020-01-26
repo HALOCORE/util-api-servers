781cda63fc85bdd45ce0383077dd0c841623cdbf
HALOCORE <wbhalo@mail.ustc.edu.cn>
2020-01-26 11:33:14 +0800

    static server working

--- node-static-server/staticapp.js
+++ node-static-server/staticapp.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 var app = express();
 
-app.use(express.static('public'));
+app.use('/static', express.static('static'));
 
 app.listen(8124 /* 端口号 */, function () {
   console.log('app is listening at port 8124');
